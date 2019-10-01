d6e0f43cd819a8c91a192836b184d0066c10faae
ospirentos <ospirentos@gmail.com>
2019-10-01 20:27:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MyButton from "./Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:"Zeynep",
+      clickMeText:"Welcome",
+      buttonState: true,
+    }
+  }
+
+  clickhandler = () => {
+    const {text, clickMeText} = this.props;
+    this.setState({
+      clickMeText: this.state.buttonState ? "Goodbye" : "Welcome",
+      buttonState: !this.state.buttonState,
+    });
+    console.log("ButtonState:", this.state.buttonState);
+  }
+
+  render() {
+    console.log("render call!")
+    return (
+      <div>
+        <div class="container">{this.state.text}</div>
+        <MyButton clr="red" />
+      </div>
+      
+    );
+  }
 }
 
 export default App;
