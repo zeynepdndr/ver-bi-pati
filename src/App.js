d2805aef87f33f19d4a177b99b36358197df1787
fatih <kocabasmu16@itu.edu.tr>
2019-12-07 15:53:10 +0300

    Started user auth

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
-import React, {Component} from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import * as ROUTES from './constants/routes';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import * as ROUTES from "./constants/routes";
 
-import Layout from './components/Layout/layout';
-import HomePage from './components/Tabs/homepage'
-import LandingPage from './components/LandingPage/landing-page';
-import Contact from './components/Tabs/contact';
-import Activities from './components/Tabs/activities';
-import Projects from './components/Tabs/projects';
-import Donation from './components/Tabs/donation';
-import Gallery from './components/Tabs/gallery';
-import Announcements from './components/Tabs/announcements';
-import Feeding from './components/Tabs/feeding';
-import AboutUs from './components/Tabs/aboutus';
-import Notification from './components/Tabs/notification';
+import Layout from "./components/Layout/layout";
+import HomePage from "./components/Tabs/homepage";
+import LandingPage from "./components/LandingPage/landing-page";
+import Contact from "./components/Tabs/contact";
+import Activities from "./components/Tabs/activities";
+import Projects from "./components/Tabs/projects";
+import Donation from "./components/Tabs/donation";
+import Gallery from "./components/Tabs/gallery";
+import Announcements from "./components/Tabs/announcements";
+import Feeding from "./components/Tabs/feeding";
+import AboutUs from "./components/Tabs/aboutus";
+import Notification from "./components/Tabs/notification";
 
-import { withFirebase } from "./components/Firebase"
+import { withFirebase } from "./components/Firebase";
 
-const LayoutFire = withFirebase(Layout)
+const LayoutFire = withFirebase(Layout);
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text:"Zeynep",
-      clickMeText:"Welcome",
-      buttonState: true,
-    }
-  }
-
-  componentDidMount() {
-  }
-
-  clickhandler = () => {
-    const {text, clickMeText} = this.props;
-    this.setState({
-      clickMeText: this.state.buttonState ? "Goodbye" : "Welcome",
-      buttonState: !this.state.buttonState,
-    });
-    console.log("ButtonState:", this.state.buttonState);
-  }
-
   render() {
-    const location = window.location.pathname;
     return (
       <div>
-          <Route exact path={ROUTES.LANDING} component={ LandingPage } />
-          <LayoutFire>
-            <Route path={ROUTES.HOME} component={ HomePage } />
-            <Route path={ROUTES.CONTACT} component={ Contact } />
-            <Route path={ROUTES.ACTIVITIES} component={ Activities } />
-            <Route path={ROUTES.PROJECTS} component={ Projects} />
-            <Route path={ROUTES.DONATION} component={ Donation } />
-            <Route path={ROUTES.GALLERY} component={ Gallery } />
-            <Route path={ROUTES.ANNOUNCEMENTS} component={ Announcements } />
-            <Route path={ROUTES.FEEDING} component={ Feeding } />
-            <Route path={ROUTES.ABOUTUS} component={ AboutUs } />
-            <Route path={ROUTES.NOTIFICATION} component={ Notification } />
-          </LayoutFire> }
-          
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <LayoutFire>
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.CONTACT} component={Contact} />
+          <Route path={ROUTES.ACTIVITIES} component={Activities} />
+          <Route path={ROUTES.PROJECTS} component={Projects} />
+          <Route path={ROUTES.DONATION} component={Donation} />
+          <Route path={ROUTES.GALLERY} component={Gallery} />
+          <Route path={ROUTES.ANNOUNCEMENTS} component={Announcements} />
+          <Route path={ROUTES.FEEDING} component={Feeding} />
+          <Route path={ROUTES.ABOUTUS} component={AboutUs} />
+          <Route path={ROUTES.NOTIFICATION} component={Notification} />
+        </LayoutFire>
+        }
       </div>
-      
     );
   }
 }
-
--- src/components/Navigation/Navbar/navbar.js
+++ src/components/Navigation/Navbar/navbar.js
@@ -1,43 +1,83 @@
-import React, {Component} from 'react';
-import './navbar.css';
-import NavigationItem from '../NavigationItem/navigationItem';
-import SignUp from './../../Auth/signup'
-import NotificationMenu from '../../Tabs/notificationMenu'
+import React, { Component } from "react";
+import "./navbar.css";
+import NavigationItem from "../NavigationItem/navigationItem";
+import SignUp from "./../../Auth/signup";
+import NotificationMenu from "../../Tabs/notificationMenu";
 
-const Navbar = (props)=>(
-  <div id="menu">
-    <nav className="navbar navbar-expand-lg">
-      <a className="navbar-brand" href="/anasayfa"><img className="logo" src={require('./../../Res/images/verBiPatiLogo.png')} alt="Ver Bi Pati"/></a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Neler Yapıyoruz?
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+class Navbar extends Component {
+  render() {
+    return (
+      <div id="menu">
+        <nav className="navbar navbar-expand-lg" style={{ margin: "40px" }}>
+          <a className="navbar-brand" href="/anasayfa">
+            <img
+              className="logo"
+              src={require("./../../Res/images/verBiPatiLogo.png")}
+              alt="Ver Bi Pati"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
               <li className="nav-item dropdown">
-                <a className="dropdown-item" href="/projeler">Projelerimiz</a>
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Neler Yapıyoruz?
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li className="nav-item dropdown">
+                    <a className="dropdown-item" href="/projeler">
+                      Projelerimiz
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="dropdown-item" href="/etkinlikler">
+                      Etkinliklerimiz
+                    </a>
+                  </li>
+                </ul>
               </li>
-              <li className="nav-item dropdown">
-                <a className="dropdown-item" href="/etkinlikler">Etkinliklerimiz</a>
-              </li>
+              <NavigationItem link="/beslemetakvimi">
+                Besleme Programı
+              </NavigationItem>
+              <NavigationItem link="/destekol">Destek Ol</NavigationItem>
+              <NavigationItem link="/ilanlar">
+                Kayıp {"&"} Sahiplendirme
+              </NavigationItem>
+              <NavigationItem link="/galeri">Galeri</NavigationItem>
+              <NavigationItem link="/iletisim">İletişim</NavigationItem>
             </ul>
-          </li>
-          <NavigationItem link="/beslemetakvimi">Besleme Programı</NavigationItem>
-          <NavigationItem link="/destekol">Destek Ol</NavigationItem>
-          <NavigationItem link="/ilanlar">Kayıp {"&"} Sahiplendirme</NavigationItem>
-          <NavigationItem link="/galeri">Galeri</NavigationItem>
-          <NavigationItem link="/iletisim">İletişim</NavigationItem>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <NotificationMenu firebase={props.firebase}></NotificationMenu>
-          <SignUp></SignUp>
-        </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <NotificationMenu
+                firebase={this.props.firebase}
+              ></NotificationMenu>
+              <SignUp firebase={this.props.firebase}></SignUp>
+              )}
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
-  </div>
-)
+    );
+  }
+}
+
 export default Navbar;
