d62149a6624eb39da42e713fda6277c1e34d20ae
topalh16 <topalh@itu.edu.tr>
2019-12-10 18:45:25 +0300

    landingpage editted

--- src/components/LandingPage/landing-page.js
+++ src/components/LandingPage/landing-page.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
 
 import './landing-page.css';
 import  CustomButton  from './../Shared/customButton'
@@ -6,10 +7,19 @@ import  CustomButton  from './../Shared/customButton'
 const LandingPage = (props)=>
 <div className="container-fluid">  
   <div className="row"> 
-    <div className="col-sm-6">      
+    <div className="col-sm-6" id="leftSide">      
+      <img id="jumbotronImage" src={require('./../Res/images/who.jpg')} alt="Ver Bi Pati"/>
       <h4 id="intro">İstanbul Teknik Üniversitesi'nde, <br/>kampüs hayvanlarının her türlü bakımını üstlenmek üzere kurulmuş <br/>gönüllük esaslı bir öğrenci kulübüyüz.</h4>
-        <CustomButton link="/anasayfa" label="Daha Fazlası" history={props.history}></CustomButton>
-    </div>
+        <Link to="/anasayfa" >    
+          <button
+            className="btn btn-default filter-button"
+            id="introButton"
+            data-filter="all"
+          >
+            DAHA FAZLASI
+          </button>
+        </Link>
+      </div>
     <div className="col-sm-6" id="rotation">
       <img id="jumbotronImage" src={require('./../Res/images/verBiPatiLogo.png')} alt="Ver Bi Pati"/>
     </div>
--- src/components/Tabs/homepageitem.js
+++ src/components/Tabs/homepageitem.js
@@ -4,7 +4,7 @@ import './homepageitem.css';
 const HomePageItem = (props) =>{
     return (
         <div className="col-sm">
-        <div className="container-fluid style_prevu_kit">
+        <div className="container-fluid">
           <img className="icon" id={props.id} src={props.imgPath} alt=""/>       
           <div className="content">
             <h6>{props.title}</h6>
