edb2362275c65d0ba3314bd61b5a6cd6cd7b39d1
mfatihkocabas <kocabasmu16@itu.edu.tr>
2019-12-15 17:54:48 +0300

    feeding table is created and modified accordingly

--- src/components/Tabs/feeding.js
+++ src/components/Tabs/feeding.js
@@ -1,44 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import CustomButton from "./../Shared/customButton";
 import "./feeding.css";
 import Map from "pigeon-maps";
 import Overlay from "pigeon-overlay";
 import FeedingSchedule from "../FeedingSchedule";
+import AddItemButton from "../Shared/addItemButton";
+import { Popover } from "antd";
 
-export default class Feeding extends Component {
-  state = {
-    view: 0
-  };
-  changeView = value => {
-    this.setState({ view: value });
-  };
-  render() {
-    const { view } = this.state;
-    return (
-      <div className="container-fluid">
-        <CustomButton
-          label="Besleme Planı"
-          onClick={() => this.changeView(0)}
-        />
-        <CustomButton label="Harita" onClick={() => this.changeView(1)} />
-        {view === 0 && <FeedingSchedule />}
-        {view === 1 && (
-          <div>
-            <Map
-              center={[41.1044, 29.0273]}
-              zoom={15.5}
-              width={800}
-              height={400}
-            >
-              <Overlay anchor={[41.1044, 29.0273]}>
-                <div>
-                  <i class="fa-li fa fa-paw" />5
-                </div>
-              </Overlay>
-            </Map>
-          </div>
-        )}
+const Feeding = () => {
+  const [view, setView] = useState(0);
+  const [editMode, setEditMode] = useState(false);
+
+  return (
+    <div>
+      <div className="centeredButton">
+        <CustomButton label="Besleme Planı" onClick={() => setView(0)} />
+        <CustomButton label="Harita" onClick={() => setView(1)} />
       </div>
-    );
-  }
-}
+      <div className="container-fluid">
+        <div className="buttonAlign">
+          <AddItemButton addActivity={() => setEditMode(!editMode)} />
+        </div>
+        <div>
+          {view === 0 && <FeedingSchedule editMode={editMode} />}
+          {view === 1 && (
+            <div>
+              <Map
+                center={[41.1044, 29.0273]}
+                zoom={15.5}
+                width={800}
+                height={400}
+              >
+                <Overlay anchor={[41.107968, 29.022134]}>
+                  <Popover title="ARI KAPI-SDKM (4 köpek)">
+                    <div>
+                      <i class="fa-li fa fa-paw" />2
+                    </div>
+                  </Popover>
+                </Overlay>
+              </Map>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feeding;
